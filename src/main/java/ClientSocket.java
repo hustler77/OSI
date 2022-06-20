6c440f275ff0ea7554156b9a835d42764521648c
Alexander Kozhin <hustler77@mail.ru>
2022-06-20 18:16:50 +0300

    first commit

---
+++ src/main/java/ClientSocket.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientSocket {
+    public static void main(String[] args) {
+        String host = "127.0.0.1";
+        int port = 8080;
+
+        try (Socket client = new Socket(host, port);
+             PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()))) {
+            out.println("Netology");
+            String resp = in.readLine();
+            System.out.println(resp);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
